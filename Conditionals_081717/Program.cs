9b3e3e599122419f5de0833ab25a171369e2bfa8
bradberger01 <bradberger@hotmail.com>
2017-08-21 18:41:28 -0400

    New Conditional Problems

--- Conditionals_081717/Program.cs
+++ Conditionals_081717/Program.cs
@@ -10,16 +10,56 @@ namespace Conditionals_081717
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your age");
+            //Console.WriteLine("Enter your age");
 
-          
-            if (age >= 17)
+            //int age = int.Parse(Console.ReadLine());
+            //if (age >= 17)
+            //{
+            //    Console.WriteLine("You can see this movie");
+            //}
+            //if(age == 18)
+            //{
+            //    Console.WriteLine("You are now an adult");
+            //        }
+            //else if (age == 21)
+            //{
+            //    Console.WriteLine("Let's get a drink");
+            //}
+
+            //else
+
+            //{
+
+            //    Console.WriteLine("Sorry you are too young you need a parent present");
+            //}
+
+            //Console.WriteLine("Enter your percentage");
+            //int grade = int.Parse(Console.ReadLine());
+            //if(grade >= 90 && grade <= 100)
+            //{
+            //    Console.WriteLine("You got an A!!");
+            //        }
+            //else if (grade >= 80 && grade <= 89)
+            //{
+            //    Console.WriteLine("You got a B!!");
+            //}
+
+            // user inputs number of people in dinner party and the total of the bill
+            Console.WriteLine("How many people were in your dining party?");
+            int dinnerParty = int.Parse(Console.ReadLine());
+            Console.WriteLine("What was the total of you bill?");
+            double dinnerBill = double.Parse(Console.ReadLine());
+
+            // computer calculates the customer appreciation discount and gives each person's total
+            if (dinnerBill >= 50)
             {
-                Console.WriteLine("You can see this movie");
+              Console.WriteLine("Congratulation, You received a 10% discount on your bill");
+              Console.WriteLine("Each person owes $" + ((dinnerBill - (dinnerBill * .10)) / dinnerParty));
+            
             }
-            else
-            {
-                Console.WriteLine("Sorry you are too young you need a parent present");
+            else if (dinnerBill < 50)
+            { Console.WriteLine("Congratulations! You received a 5% discount on your bill");
+              Console.WriteLine("Each person owes $" + ((dinnerBill - (dinnerBill * .05)) / dinnerParty));
             }
         }
     }
